cc5449b8333c8570d206a303799ecc4287b5feca
Franklin Barto <kipkalisbartoo@gmail.com>
2024-08-19 15:50:55 +0530

    wireframe complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import HomePage from './Pages/homepage.jsx';
+import AnalysisPage from './Pages/analysis';
+import VisualisationPage from './Pages/visualisation';
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/analysis" element={<AnalysisPage />} />
+        <Route path="/visualization" element={<VisualisationPage />} />
+      </Routes>
+    </Router>
   );
 }
 
